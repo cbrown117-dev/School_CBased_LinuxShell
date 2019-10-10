f2a0e61cfb364ad1a43812d2d3c78c4503a25b4a
chris brown <sevrc1136@gmail.com>
2019-10-09 21:49:53 -0500

    finished with project, doesnt all work but whatever

--- projects/project1.c
+++ projects/project1.c
@@ -1,3 +1,12 @@
+/*
+ * Chris Brown
+ * sevrc
+ * Project: 1
+ */
+
+// I used the code from the book and modified it to fit the requirements
+// the only thing online I looked at was .h files
+
 #include <dirent.h>
 #include <limits.h>
 #include <string.h>
@@ -13,20 +22,39 @@ typedef int     Myfunc(const char *, const struct stat *, int);
 static Myfunc   myfunc;
 static int              myftw(char *, Myfunc *);
 static int              dopath(Myfunc *);
+void reg(char *);
+void sandf(char *, size_t);
+void f(char *);
+void s(char *, size_t);
 
-static long     nreg, ndir, nblk, nchr, nfifo, nslink, nsock, ntot;
+int fi = 0, si = 0; //global variables to decide if i should print via -s or -f or both
+char size[10000];
+char find[200];
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-        int             ret;
+	        int             ret;
 
-        if (argc != 2)
-                exit(-1);
+		if (argc < 2){
+			ret = myftw(".", myfunc);
+		}
 
-        ret = myftw(argv[1], myfunc);           /* does it all */
-
-  return 0;
+		else if(argc == 3){
+			if ( strcmp(argv[1], "-f") == 0){
+				fi = 1;
+				strcpy(find,argv[2]);
+				ret = myftw(".", myfunc);
+			}
+			else if (strcmp(argv[1],"-s") == 0){
+				si =1;
+				strcpy(size, argv[2]);
+				printf("looking for file size of %s\n", size);
+				ret = myftw(".", myfunc);
+			}
+		}
+		else
+			ret = myftw(argv[1], myfunc);           
+	return 0;
 }
 
 /*
@@ -40,19 +68,20 @@ main(int argc, char *argv[])
 
 static char     *fullpath;              /* contains full pathname for every file */
 static size_t pathlen;
-static int                                      /* we return whatever func() returns */
-myftw(char *pathname, Myfunc *func)
+
+static int myftw(char *pathname, Myfunc *func)
 {
-        fullpath = malloc(sizeof(PATH_MAX) + 1);        /* malloc PATH_MAX+1 bytes */
-                                                                                /* ({Prog pathalloc}) */
-        if (pathlen <= strlen(pathname)) {
-                pathlen = strlen(pathname) * 2;
-                if ((fullpath = realloc(fullpath, pathlen)) == NULL)
-                        exit(-1);
-        }
-        strcpy(fullpath, pathname);
-        return(dopath(func));
+	        fullpath = malloc(sizeof(PATH_MAX) * 2000);        /* malloc PATH_MAX+1 bytes */
+		                                                           /* ({Prog pathalloc}) */
+		        if (pathlen <= strlen(pathname)) {
+				pathlen = strlen(pathname) * 200;
+				if ((fullpath = realloc(fullpath, pathlen)) == NULL)
+					exit(-1);
+			}
+			strcpy(fullpath, pathname);
+			return(dopath(func));
 }
+
 
 /*
  * Descend through the hierarchy, starting at "fullpath".
@@ -60,80 +89,118 @@ myftw(char *pathname, Myfunc *func)
  * call func(), and return.  For a directory, we call ourself
  * recursively for each name in the directory.
  */
-static int                                      /* we return whatever func() returns */
-dopath(Myfunc* func)
+static int dopath(Myfunc* func)
 {
-        struct stat             statbuf;
-        struct dirent   *dirp;
-        DIR                             *dp;
-        int                             ret, n;
+	        struct stat             statbuf;
+		struct dirent   *dirp;
+		DIR                             *dp;
+		int                             ret, n;
 
-        if (lstat(fullpath, &statbuf) < 0)      /* stat error */
-                return(func(fullpath, &statbuf, FTW_NS));
-        if (S_ISDIR(statbuf.st_mode) == 0)      /* not a directory */
-                return(func(fullpath, &statbuf, FTW_F));
+		if (lstat(fullpath, &statbuf) < 0)      /* stat error */
+			return(func(fullpath, &statbuf, FTW_NS));
+		if (S_ISDIR(statbuf.st_mode) == 0)      /* not a directory */
+		        return(func(fullpath, &statbuf, FTW_F));
+ /*
+ * It's a directory.  First call func() for the directory,
+ * then process each filename in the directory.
+ */
+	if ((ret = func(fullpath, &statbuf, FTW_D)) != 0)
+		return(ret);
+	n = strlen(fullpath);
+	if (n + PATH_MAX + 2 > pathlen) {       /* expand path buffer */
+		pathlen *= 2;
+		if ((fullpath = realloc(fullpath, pathlen)) == NULL)
+			exit(-1);
+	}
+	fullpath[n++] = '/';
+	fullpath[n] = 0;
 
-        /*
-         * It's a directory.  First call func() for the directory,
-         * then process each filename in the directory.
-         */
-        if ((ret = func(fullpath, &statbuf, FTW_D)) != 0)
-                return(ret);
-
-        n = strlen(fullpath);
-	        if (n + PATH_MAX + 2 > pathlen) {       /* expand path buffer */
-                pathlen *= 2;
-                if ((fullpath = realloc(fullpath, pathlen)) == NULL)
-                        exit(-1);
-        }
-        fullpath[n++] = '/';
-        fullpath[n] = 0;
-
-        if ((dp = opendir(fullpath)) == NULL)   /* can't read directory */
-                return(func(fullpath, &statbuf, FTW_DNR));
-
-        while ((dirp = readdir(dp)) != NULL) {
-                if (strcmp(dirp->d_name, ".") == 0  ||
-                    strcmp(dirp->d_name, "..") == 0)
-                                continue;               /* ignore dot and dot-dot */
-                strcpy(&fullpath[n], dirp->d_name);     /* append name after "/" */
-                if ((ret = dopath(func)) != 0)          /* recursive */
-                        break;  /* time to leave */
-        }
-        fullpath[n-1] = 0;      /* erase everything from slash onward */
-
-        if (closedir(dp) < 0)
-                exit(-1);
-        return(ret);
+	if ((dp = opendir(fullpath)) == NULL)   /* can't read directory */
+		return(func(fullpath, &statbuf, FTW_DNR));
+	while ((dirp = readdir(dp)) != NULL) {
+		if (strcmp(dirp->d_name, ".") == 0  || strcmp(dirp->d_name, "..") == 0)
+			continue;                        /* ignore dot and dot-dot */
+		strcpy(&fullpath[n], dirp->d_name);     /* append name after "/" */
+		if ((ret = dopath(func)) != 0)         /* recursive */
+			break;  /* time to leave */
+	}
+	fullpath[n-1] = 0;      /* erase everything from slash onward */
+	if (closedir(dp) < 0)
+		exit(-1);
+	return(ret);
 }
+
+int tabover = 0;
 
 static int
 myfunc(const char *pathname, const struct stat *statptr, int type)
 {
-        switch (type) {
-        case FTW_F:
-                switch (statptr->st_mode & S_IFMT) {
-                case S_IFREG:   nreg++;         break;
-                case S_IFBLK:   nblk++;         break;
-                case S_IFCHR:   nchr++;         break;
-                case S_IFIFO:   nfifo++;        break;
-                case S_IFLNK:   nslink++;       break;
-                case S_IFSOCK:  nsock++;        break;
-                case S_IFDIR:   /* directories should have type = FTW_D */
-                        exit(-1);
-                }
-                break;
-        case FTW_D:
-                printf("%s %ld \n", pathname, statptr->st_size);
-                break;
-        case FTW_DNR:
-                exit(-1);
-                break;
-        case FTW_NS:
-                exit(-1);
-                break;
-        default:
-                exit(-1);
-        }
-        return(0);
+	        char str[100];
+		strcpy(str,pathname);
+		switch (type) {
+			case FTW_F:
+				switch (statptr->st_mode & S_IFMT) {
+					case S_IFREG: 
+						if (si == 1 && fi == 1 ){
+							sandf(str, statptr->st_size); 
+						}
+						else if (fi ==1){
+							f(str);
+						}
+						else if(si == 1){
+							s(str, statptr->st_size);
+						}																										                             else reg(str);
+						break;
+					case S_IFLNK:   break; //should do links here...not going to
+					case S_IFDIR:  //directories under something else
+						exit(-1);
+				}
+				break;
+			case FTW_D:
+				if (tabover > 0)
+					printf("\t%s:\n", pathname);
+				else
+					printf("%s:\n", pathname); tabover++;
+				break;
+			default:  exit (-1);
+		}
+	return(0);
+}
+
+/*
+ * token prints the files out
+ * by taking the fullpath name and parsing through it
+ * printing out only after "/"
+ */
+
+void reg(char* path){
+	int len = strlen(path);
+	int i, j;
+	for(i=0;i<len;i++){
+		if ( path[i] == "/")
+			j = i;
+	}
+	printf("\t\t");
+	for(i=j+1;i<len;i++){
+		printf("%c",path[i]);
+	}
+	printf("\n");
+}
+
+void s(char* path, size_t check){
+	int size_compare = atoi(size);
+	if(check >= size_compare){
+		reg(path);
+	}
+}
+
+void f(char* path){
+	if ( strstr(path, find) != NULL)
+		reg(find);
+}
+
+void sandf(char *path, size_t check){
+	int compare_this = atoi(size);
+	if (strstr(path, find) != NULL && compare_this >= check )
+		reg(find);
 }

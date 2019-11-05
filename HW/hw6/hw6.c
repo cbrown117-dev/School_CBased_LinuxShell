91becc56af66abb32d97392cbcf0e8972853924c
chris <sevrc1136@gmail.com>
2019-11-05 11:43:11 -0600

    fixed hw6

--- HW/hw6/hw6.c
+++ HW/hw6/hw6.c
@@ -31,29 +31,29 @@ struct listing getfields(char* line){
    return item;
 }
 
-void displayStruct(struct listing items){
-        printf("%d\t%d\t%s\t%s\t%s\t%lf\t%lf\t%s\t%f\n",items.id,items.host_id,items.host_name,items.neighbourhood_group,items.neighbourhood,items.latitude,
+void displayStruct(struct listing items, FILE *fp){
+        fprintf(fp,"ID:%d\n Host Id:%d\n Host Name:%s\n Neighbourhood Group:%s\n Neighbourhood:%s\n Latitude:%lf\n Longitude:%lf\n Room Type:%s\n Price:%f\n",items.id,items.host_id,items.host_name,items.neighbourhood_group,items.neighbourhood,items.latitude,
                                                         items.longitude,items.room_type,items.price);
 }
 
-void sortHost(struct listing item[22555], int len){
+void sortHost(struct listing item[22520], int len){
         int i,j;
-
-        for(j=1;j<len;j++){
-                char key[50];
+        for(j=1;j<len-1;j++){
+                char key[20000];
                 strcpy(key, item[j].host_name);
                 i = j-1;
-                while(i >= 0 && (strcmp(item[i].host_name,key) > 0) ){
+                while(i >= 0 && (strcmp(item[i].host_name, key) > 0) ){
                         strcpy(item[i+1].host_name,item[i].host_name);
                         i--;
                 }
+		
                 strcpy(item[i+1].host_name, key);
         }
 }
 
-void sortPrice(struct listing item[22555], int len){
+void sortPrice(struct listing item[22520], int len){
         int i,j;
-        for(j=1;j<len;j++){
+        for(j=1;j<len-1;j++){
                 float key;
                 key = item[j].price;
                 i = j-1;
@@ -66,40 +66,47 @@ void sortPrice(struct listing item[22555], int len){
 }
 
 int main(int argc, char **argv){
-    int i, count;
-    int LINESIZE = 1024;
-    char line[LINESIZE];
-    struct listing list_items[22555];
+    	int i, count;
+    	int LINESIZE = 2000;
+    	char line[LINESIZE];
+    	struct listing list_items[22520];
 
-    FILE *fp = fopen(argv[1], "r");
-    FILE *fp1 = fopen(argv[2], "w");
+    	FILE *fp = fopen(argv[1], "r");
+    	FILE *fp1 = fopen(argv[2], "w");
+	FILE *fp3 = fopen(argv[2], "w");
 
-    count = 0;
-    while (fgets(line, LINESIZE, fp) != NULL){
-       list_items[count++] = getfields(line);
-    }
+    	count = 0;
+    	while (fgets(line, LINESIZE, fp) != NULL){
+       		list_items[count++] = getfields(line);
+    	}	
 
-    printf("printing list with sorted hosts names\n");
-    sortHost(list_items, 22555);
-
-    printf("id\thost_id\thost_name\tneighbourhood_group\tneighbourhood\tlatitude\tlongitude\troom_type\tprice\n");
-    for (i=0; i<count; i++)
-       displayStruct(list_items[i]);
-
-    printf("soring via price now\n");
-    sortPrice(list_items, 22555);
-
-    for(i=0;i<count;i++){
-        displayStruct(list_items[i]);
-    }
+    	printf("printing list with sorted hosts names\n");
+   	sortHost(list_items, 22520);
+	printf("out of sortHost()\n");
+	printf("printing to writeto.txt\n");
 
     i = 0;
+    fprintf(fp1,"By name\n");
     while(i != count){
-        fputs(list_items[i].host_name, fp1);
-        fprintf(fp1, "%f", list_items[i].price);
+        displayStruct(list_items[i], fp1);
+	i++;
     }
 
     fclose(fp);
+
+    printf("sorting via price now\n");
+    sortPrice(list_items, 22520);
+    printf("done with sorting\n");
+
+    i = 0;
+    fprintf(fp3,"By Price\n");
+    while(i != count){
+        displayStruct(list_items[i], fp3);
+	i++;
+    }
+
+
+    fclose(fp3);
 
 return 0;
 }
